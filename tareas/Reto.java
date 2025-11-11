2fc22102050ae24c9c348ac78aefc1f670fca93d
brayanhnnn <brayanhn508@gmail.com>
2025-11-10 20:41:51 -0600

    Update Reto.java

--- tareas/Reto.java
+++ tareas/Reto.java
@@ -1,5 +1,27 @@
 package tareas;
 
-public class Reto {
-    
+public class Reto extends Tareas {
+    private boolean completado;
+
+    public Reto(String titulo, String info, String categoria, String duracion) {
+        super(titulo, info, categoria, duracion);
+        this.completado = false;
+    }
+
+    public void marcarCompletado() {
+        this.completado = true;
+    }
+
+    public boolean isCompletado() {
+        return completado;
+    }
+
+    @Override
+    public void mostrarInfo() {
+        System.out.println("Reto: " + titulo);
+        System.out.println("Descripción: " + info);
+        System.out.println("Categoría: " + categoria);
+        System.out.println("Duración: " + duracion);
+        System.out.println("Estado: " + (completado ? "Completado" : "En progreso"));
+    }
 }
