972ad0d81a83eda7eaffbcb7d6b9d4eeaf6c99f8
brayanhnnn <brayanhn508@gmail.com>
2025-11-10 20:43:35 -0600

    Update Tareas.java

--- tareas/Tareas.java
+++ tareas/Tareas.java
@@ -5,4 +5,29 @@ public abstract class Tareas {
     protected String info;
     protected String categoria;
     protected String duracion;
+
+    public Tareas(String titulo, String info, String categoria, String duracion) {
+        this.titulo = titulo;
+        this.info = info;
+        this.categoria = categoria;
+        this.duracion = duracion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public abstract void mostrarInfo();
 }
