e942b7650e708aff1f911a87018dbbcc70045193
chevimeneses <chevimeneses1377@gmail.com>
2025-11-10 19:56:06 -0600

    Versión con Recompensas (a medias xd)

---
+++ recompensas/Recompensa.java
@@ -0,0 +1,36 @@
+package recompensas;
+
+import usuarios.Cliente;
+
+public abstract class Recompensa {
+
+    protected String nombre;
+    protected int costo;
+    protected String tipo;
+
+    public Recompensa(String nombre, String descripcion, int costo, String tipo) {
+        this.nombre = nombre;
+        this.costo = costo;
+        this.tipo = tipo;
+    }
+
+    public String getTipo() { 
+        return tipo; 
+    }
+
+    public String getNombre() { 
+        return nombre; 
+    }
+
+    public int getCosto() { 
+        return costo; 
+    }
+
+
+    public void canjear(Cliente cliente) {
+        aplicar(cliente);
+        cliente.restarPuntos(costo);
+    }
+
+    public abstract void aplicar(Cliente cliente);
+}
