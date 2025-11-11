e8e2f3bb8a8bdab2faee1de7e2ce7e9554fc3995
ebergdx <garcia.sanchez.eber453@gmail.com>
2025-11-10 20:09:36 -0600

    Update Admin.java

--- usuarios/Admin.java
+++ usuarios/Admin.java
@@ -2,8 +2,8 @@ package usuarios;
 
 public class Admin extends Usuario {
     private short clave;
-    
-    public Admin(String nombre, String email, int id, String fechaNacimiento, int puntos, short clave) {
+
+    public Admin(String nombre, String email, int id, short clave) {
         super(nombre, email, id);
         this.clave = clave;
     }
