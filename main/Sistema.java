257ba2eb789e19e05350b7e3a04a1565a1555609
ebergdx <garcia.sanchez.eber453@gmail.com>
2025-11-18 20:50:13 -0600

    Update Sistema.java

--- main/Sistema.java
+++ main/Sistema.java
@@ -1,143 +1,169 @@
 package main;
 
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import java.util.Set;
 
-import excepciones.CorreoExistenteException;
 import usuarios.*;
+import java.io.*;
+import tareas.*;
+import java.util.ArrayList;
 
-public class Sistema {
-    private static Scanner sc = new Scanner(System.in);
-    public static ObjectOutputStream usuarios;
-    public static Set<String> emails;
-    public static void main(String[] args) {
-        System.out.println("- Bienvenido a la aplicación para registrar hábitos -");
-        int opc;
+import excepciones.UsuarioNoEncontrado;
+import recompensas.Recompensa;
 
-        do {
-            System.out.println("1)Iniciar sesión\n2)Crear cuenta\n0)Salir");
-            opc = sc.nextInt();
-            sc.nextLine();
+public class ControlArchivos {
+    public static final String CLIENTES_REGISTRO = "usuarios.bin";
+    public static final String ADMINS_REGISTRO = "admins.bin";
+    public static final String RECOMPENSAS = "recompensas.bin";
+    public static final String HABITOS = "habitos.bin";
 
-            switch(opc) {
-                case 1 -> iniciarSesion();
-                case 2 -> crearCuenta();
-                case 3 -> System.out.println("Saliendo del sistema...");
-                default -> System.out.println("Ingrese una opción válida");
-            }
-        } while(opc != 0);
-    }
-
-    private static void iniciarSesion() {
-        int i = 3;
-        Usuario ingresoUsuario;
-
-        System.out.println("\t|Ingrese los datos de su cuenta|\n");
-        do {
-            try {
-                System.out.println("Email: ");
-                String correo = sc.nextLine();
-                System.out.println("Contraseña: ");
-                int contrasenia = sc.nextInt();
-
-                System.out.println("1)Sí\notro)No\n¿Es administrador?: ");
-                int opc = sc.nextInt();
-                sc.nextLine();
-
-                if(opc == 1) {
-                    System.out.println("Ingrese su clave: ");
-                    Short clave = sc.nextShort();
-                    
-                } else {
-
-                }
-
-
-
-                break;
-            } catch(InputMismatchException e) {
-                i--;
-                sc.nextLine();
-                System.out.println("Excepción: Solo puede usar números enteros para la contraseña.");
-                System.out.println("\n\t- Inténtelo nuevamente, le quedan" + i +  "intentos -");
-            }
-        } while(i > 0);
-
-        if(i == 0) {
-            System.out.println("Contraseña incorrecta.");
-            return;
-        }
-    }
-
-    private static void crearCuenta() {
-        int i = 3;
-    
-        System.err.println("\t|Por favor, ingrese los siguientes datos para crear su cuenta|\n");
-        do {
-            try {
-                System.out.println("Nombre: ");
-                String nombre = sc.nextLine();
-                System.out.println("Correo: ");
-                String email = sc.nextLine();
-                correoExistente(email);
-
-                System.out.println("Contraseña: ");
-                int contrasenia = sc.nextInt();
-                emails.add(email);
-                break;
-            } catch(InputMismatchException e) {
-                i--;
-                sc.nextLine();
-                System.out.println("Excepción: Solo puede usar números enteros para la contraseña.");
-                System.out.println("\n\t- Inténtelo nuevamente, le quedan" + i +  "intentos -");
-            }
-        } while(i > 0);
-
-        if(i == 0) {
-            System.out.println("Contraseña incorrecta.");
-            return;
-        }
-
-        System.out.println("1)Sí\2)No\n¿Es administrador?");
-        int opc = sc.nextInt();
-
-        Usuario nuevoUsuario;
-        if(opc == 1) {
-            int j = 3;
-            do {
-                try {
-                    System.out.println("Ingrese su clave: ");
-                    short clave = sc.nextShort();
-                    nuevoUsuario = new Admin(nombre, email, contrasenia, clave);
-                    break;
-                } catch(InputMismatchException e) {
-                    j--;
-                    System.out.println("Ingrese un número entero para su clave.");
-                } catch(CorreoExistenteException e) {
-                    j--;
-                    e.printStackTrace();
-                }
-            } while(j > 0);
-        } else {
-            nuevoUsuario = new Cliente(nombre, email, contraseña);
-        }
-
+    public static void guardarClientes(ArrayList<Usuario> clientes) {
         try {
-            usuarios.writeObject(nuevoUsuario);
-            usuarios.close();
+            OutputStream os = new FileOutputStream(CLIENTES_REGISTRO);
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            oos.writeObject(clientes);
+            oos.close();
         } catch(IOException e) {
-            System.out.println("Excepción: Archivo no encontrado.");
+            System.out.println("Excepción: Usuarios no almacenados correctamente.");
+            e.printStackTrace();
         }
     }
 
-    public static void correoExistente(String email) throws CorreoExistenteException {
-        for(String correoExistente : emails) {
-            if(correoExistente.equals(email)) {
-                throw new CorreoExistenteException();
+    public static ArrayList<Cliente> cargarClientes() {
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        try {
+            InputStream is = new FileInputStream(CLIENTES_REGISTRO);
+            ObjectInputStream ois = new ObjectInputStream(is);
+            clientes = (ArrayList<Cliente>) ois.readObject();
+            ois.close();
+            is.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo de clientes no encontrado.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Excepción: Archivo de usuarios no leído.");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Excepción: Archivo de clientes no leído.");
+            e.printStackTrace();
+        } catch(ClassCastException e) {
+            System.out.println("Excepción: Error de casteo.");
+            e.printStackTrace();
+        }
+        return clientes;
+    }
+
+    public static void verClientes(ArrayList<Usuario> usuarios) {
+        try {
+            InputStream is = new FileInputStream(CLIENTES_REGISTRO);
+            ObjectInputStream ois = new ObjectInputStream(is);
+            
+            Usuario usuario = (Usuario)ois.readObject();
+
+            for(int i = 0; i < usuarios.size(); i++) {
+                System.out.println(usuario);
             }
+            
+            ois.close();
+        } catch(IOException e) {
+            System.out.println("Excepción: Archivo no leído.");
+            e.printStackTrace();
+        } catch(ClassNotFoundException e) {
+            System.out.println("Excepción: Clase no encontrada.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void guardarAdmins(ArrayList<Usuario> admins) {
+        try {
+            OutputStream os = new FileOutputStream(ADMINS_REGISTRO);
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            oos.writeObject(admins);
+            oos.close();
+        } catch(IOException e) {
+            System.out.println("Excepción: Usuarios no almacenados correctamente.");
+        }
+    }
+
+    public static ArrayList<Admin> cargarAdmins() {
+        ArrayList<Admin> admins = new ArrayList<>();
+        try {
+            InputStream is = new FileInputStream(ADMINS_REGISTRO);
+            ObjectInputStream ois = new ObjectInputStream(is);
+            admins = (ArrayList<Admin>) ois.readObject();
+            ois.close();
+            is.close();
+        } catch(FileNotFoundException e) {
+            System.out.println("Archivo de usuarios no encontrado.");
+            e.printStackTrace();
+        } catch(IOException e) {
+            System.out.println("Excepción: Archivo de usuarios no leído.");
+            e.printStackTrace();
+        } catch(ClassNotFoundException e) {
+            System.out.println("Excepción: Archivo de clientes no leído.");
+            e.printStackTrace();
+        } catch(ClassCastException e) {
+            System.out.println("Excepción: Error de casteo.");
+            e.printStackTrace();
+        }
+        return admins;
+    }
+
+    public static void guardarRecompensas(ArrayList<Recompensa> recompensas) {
+        try {
+            OutputStream os = new FileOutputStream(RECOMPENSAS);
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            oos.writeObject(recompensas);
+            oos.close();
+        } catch(IOException e) {
+            System.out.println("Excepción: Usuarios no almacenados correctamente.");
+        }
+    }
+
+    public static ArrayList<Recompensa> cargarRecompensas() {
+        ArrayList<Recompensa> recompensas = new ArrayList<>();
+        
+        try {
+            InputStream is = new FileInputStream(RECOMPENSAS); 
+            ObjectInputStream ois = new ObjectInputStream(is);
+            recompensas = (ArrayList<Recompensa>) ois.readObject(); 
+            ois.close();
+            is.close();
+        } catch(FileNotFoundException e) {
+            System.out.println("Archivo de usuarios no encontrado.");
+            e.printStackTrace();
+        } catch(IOException e) {
+            System.out.println("Excepción: Archivo de usuarios no leído.");
+            e.printStackTrace();
+        } catch(ClassNotFoundException e) {
+            System.out.println("Excepción: Archivo de clientes no leído.");
+            e.printStackTrace();
+        } catch(ClassCastException e) {
+            System.out.println("Excepción: Error de casteo.");
+            e.printStackTrace();
+        }
+        return recompensas;
+    }
+
+    public static void verClientes() {
+        ArrayList<Cliente> clientes = cargarClientes();
+        if(clientes.isEmpty()) {
+            System.out.println("No hay clientes registrados.");
+            return;
+        }
+
+        for(Cliente cliente : clientes) {
+            System.out.println(cliente);
+        }
+    }
+
+    public static void verAdmins() {
+        ArrayList<Admin> admins = cargarAdmins();
+        if(admins.isEmpty()) {
+            System.out.println("No hay clientes registrados.");
+            return;
+        }
+
+        for(Admin admin : admins) {
+            System.out.println(admins);
         }
     }
 }
