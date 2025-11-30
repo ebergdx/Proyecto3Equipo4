ba8ab3f4b2a5dcc4a2639ee183a8887b214d9b7a
brayanhnnn <brayanhn508@gmail.com>
2025-11-30 15:20:31 -0600

    Create ObservadorEstadisticas.java

---
+++ eventos/ObservadorEstadisticas.java
@@ -0,0 +1,25 @@
+package eventos;
+
+public class ObservadorEstadisticas implements ObservadorEventos {
+    private int totalPuntosRepartidos = 0;
+    private int totalTareasCompletadas = 0;
+    
+    @Override
+    public void eventoGenerado(EventoUsuario evento) {
+        switch (evento.getTipo()) {
+            case EventoUsuario.habitoCompletado:
+            case EventoUsuario.eventoTerminado:
+            case EventoUsuario.retoCompletado:
+                totalTareasCompletadas++;
+                break;
+            case EventoUsuario.puntosGanados:
+                totalPuntosRepartidos += evento.getPuntos();
+                break;
+        }
+        
+        if (totalTareasCompletadas % 5 == 0) {
+            System.out.println("Estadisticas: " + totalTareasCompletadas + 
+                             " tareas completadas, " + totalPuntosRepartidos + " puntos repartidos");
+        }
+    }
+}
